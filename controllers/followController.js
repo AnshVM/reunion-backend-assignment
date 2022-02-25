e4ec056db5a21851a95e229effd45fcba46bcf72
AnshVM <ansh@DESKTOP-HOD6E4P.localdomain>
2022-02-25 20:36:46 +0530

    Unfollow endpoint complete

--- controllers/followController.js
+++ controllers/followController.js
@@ -18,3 +18,38 @@ exports.follow = async(req,res) => {
         res.status(500).json(err)
     }
 }
+
+
+exports.unfollow = async(req,res) => {
+    const followedId = Number(req.params.id);
+    const followerId = Number(req.id);
+
+    try{
+        const follow = await Follow.findOne({
+            where:{
+                followedId,
+                followerId
+            }
+        })
+        await follow.destroy()
+
+        const unfollowed = await User.findOne({
+            where:{id:followedId}
+        })
+        unfollowed.followers--
+
+        const prevFollower = await User.findOne({
+            where:{id:followerId}
+        })
+        prevFollower.following--
+
+        await unfollowed.save()
+        await prevFollower.save()
+
+        res.status(200).json({unfollowed,prevFollower})
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json(err)
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const Router = require('express').Router();
 const {register,authenticate} = require('../controllers/userController')
 const {auth} = require('../auth')
-const {follow} = require('../controllers/followController')
+const {follow,unfollow} = require('../controllers/followController')
 
 Router.post('/users',register);
 Router.post('/authenticate',authenticate)
 Router.post('/follow/:id',auth,follow)
+Router.post('/unfollow/:id',auth,unfollow)
+
 
 module.exports = Router;
